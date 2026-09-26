d53a661313e1607ca48bae5ccccc7ff4ed209a79
DELOLCAT <ahmad.ayan2012@gmail.com>
2026-09-26 17:41:07 +0530

    small typo

--- build-web.sh
+++ build-web.sh
@@ -1,6 +1,6 @@
 set -euo pipefail # using nothing special, so its fine to use what the community calls "strict mode"
 
-wasm-pack build -target web -- --features wasm
+wasm-pack build --target web -- --features wasm
 mkdir -p web/src/lib/pkg # TODO make this better ig
 rm -r web/src/lib/pkg
 mv pkg web/src/lib/pkg 
